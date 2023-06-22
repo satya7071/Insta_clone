29c8071ec1e4ba501d81df48a41812334360cb16
Satya2553 <mareedu..satyanarayana.5475@gmail.com>
2023-06-22 12:56:21 +0530

    deployment

--- social_media/settings.py
+++ social_media/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'media',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'social_media.urls'
@@ -127,3 +129,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media_images/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_HEADERS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_WHITELIST = ['*']
+CORS_TRUSTED_ORIGINS = ['*']
