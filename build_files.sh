1a90c7fbac2deedc115f929e8bdeaf2a66d95ce5
Satya2553 <mareedu..satyanarayana.5475@gmail.com>
2023-07-15 12:10:47 +0530

    hosting

--- build_files.sh
+++ build_files.sh
@@ -5,10 +5,10 @@ apt-get update
 apt-get install -y default-libmysqlclient-dev
 
 # Upgrade pip and setuptools
-pip install --no-cache-dir --upgrade pip setuptools
+python3.9 -m pip install --no-cache-dir --upgrade pip setuptools
 
 # Install Python dependencies
-pip install --no-cache-dir -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Install mysqlclient
 apt-get install -y libmariadb-dev
@@ -18,3 +18,5 @@ export MYSQL_UNIX_PORT="/tmp/mysql.sock"
 
 # Run database migrations (if applicable)
 python manage.py migrate
+
+python3.9 manage.py collectstatic --noinput --clear
