49275c9483f53af2aca7416d7f2f4f87e72ca2bc
Satya2553 <mareedu..satyanarayana.5475@gmail.com>
2023-07-15 12:06:44 +0530

    hosting

--- build_files.sh
+++ build_files.sh
@@ -4,12 +4,17 @@
 apt-get update
 apt-get install -y default-libmysqlclient-dev
 
-# Create and activate a virtual environment (optional)
-python -m venv venv
-source venv/bin/activate
-
 # Upgrade pip and setuptools
 pip install --no-cache-dir --upgrade pip setuptools
 
 # Install Python dependencies
 pip install --no-cache-dir -r requirements.txt
+
+# Install mysqlclient
+apt-get install -y libmariadb-dev
+
+# Set environment variables
+export MYSQL_UNIX_PORT="/tmp/mysql.sock"
+
+# Run database migrations (if applicable)
+python manage.py migrate
