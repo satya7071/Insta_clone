2997f419fceffd0d13dd2c5c76c26e41325f1fc5
Satya2553 <mareedu..satyanarayana.5475@gmail.com>
2023-07-15 11:33:36 +0530

    hosting

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,15 @@
-# build_files.sh
+#!/bin/bash
 
+# Install necessary system-level dependencies
+apt-get update
 apt-get install -y default-libmysqlclient-dev
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Create and activate a virtual environment (optional)
+python -m venv venv
+source venv/bin/activate
+
+# Install Python dependencies
+pip install --no-cache-dir -r requirements.txt
+
+# Install mysqlclient
+pip install mysqlclient
